9a1e8cd183dcdb315abcc1466233ec34a2377493
yawar2019 <yawarali17@gmail.com>
2021-03-20 16:46:34 +0530

    committed

---
+++ AdminEcomerceVersion_2/Controllers/TagsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminEcomerceVersion_2.Controllers
+{
+    public class TagsController : Controller
+    {
+        // GET: Tags
+        public ActionResult ShowTag()
+        {
+            return View();
+        }
+    }
+}
