92ffaccd735a79ca20be1b1cf818f21c60bdedce
yawar2019 <yawarali17@gmail.com>
2021-03-23 02:08:39 +0530

    commited code

--- AdminEcomerceVersion_2/Controllers/AdvertisementController.cs
+++ AdminEcomerceVersion_2/Controllers/AdvertisementController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AdminEcomerceVersion_2.Models;
+using ecomserv.Ecom;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,32 @@ namespace AdminEcomerceVersion_2.Controllers
 {
     public class AdvertisementController : Controller
     {
+        Configuration config = new Configuration();
         // GET: Advertisement
         public ActionResult addAdvertisement()
         {
             return View();
+        }
+
+        public ActionResult getAdvertisement()
+        {
+            return View();
+        }
+
+        public ActionResult getAdvertisementDetail(int? id)
+        {
+            ViewBag.addsDetails = id;
+            return View();
+        }
+
+        public JsonResult getAdvertisementJson()
+        {
+            EcomData ecom_data = config.ConfigureEcomData("get_ads_home");
+            my_data my_data = new my_data();
+            my_data.dev_id = ecom_data.dev_id;
+            ecom_data.data = Newtonsoft.Json.JsonConvert.SerializeObject(my_data);
+            var routes_list = config.CongifuretoJson(ecom_data);
+            return Json(routes_list, JsonRequestBehavior.AllowGet);
         }
     }
 }
